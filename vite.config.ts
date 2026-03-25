6f634033e40b274be2c81c87a0af57efd8d6d4ed
alina <alina.lukianchenkova@tages.ru>
2026-03-25 14:42:12 +0300

    Начало работы. Создание проекта

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api/auth': {
+        target: 'http://91.220.155.234:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/auth/, '/FrontTestingService-auth'),
+      },
+      '/api': {
+        target: 'http://91.220.155.234:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/FrontTestingService-back'),
+      }
+    }
+  }
+})
